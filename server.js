feebe2361f9a756bb3a6c5ec7519310da7bfe938
Murilo D. <129670228+self1027@users.noreply.github.com>
2025-02-22 00:46:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
 
 // Configuração do banco de dados
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Pode ser necessário para conexões externas
   },
 });
 
 app.use(express.json());
+
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM usuarios");
@@ -23,4 +22,8 @@ app.get("/", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Erro ao buscar usuários", error: error.message });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
