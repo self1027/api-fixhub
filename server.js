f11c4772a406ee0e1d2ba7f08c257245839a385d
Murilo D. <129670228+self1027@users.noreply.github.com>
2025-02-22 00:44:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
+// Configuração do banco de dados
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({ message: "API rodando na Cloud Run!!!" });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM usuarios");
+    res.json({ message: "🚀 API rodando na Cloud Run!", usuarios: result.rows });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar usuários", error: error.message });
+  }
 });
