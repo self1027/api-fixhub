1776837cd5d709a63f3f44a4b6998b5e034b86ee
Murilo D. <129670228+self1027@users.noreply.github.com>
2025-02-22 00:20:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "🚀 API rodando na Cloud Run!!" });
-});
-
-app.listen(PORT, () => {
-  console.log(Servidor rodando na porta ${PORT});
 });
