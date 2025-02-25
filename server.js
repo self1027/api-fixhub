06c7adaa1810c959b00ebd3abe1391c0cad123ec
Murilo Dias <diasmurilo02@gmail.com>
2025-02-25 20:27:13 -0300

    Cadastro Imagens V1.1.1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.set("trust proxy", 1);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/cadastro", cadastroRoutes);
 app.use("/login", loginRoutes);
