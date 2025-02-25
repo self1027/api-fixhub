9e45c4952584d7a5559df23c1d8b1312594e893c
Murilo Dias <diasmurilo02@gmail.com>
2025-02-25 18:15:44 -0300

    Adicionado Import para funcionar

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cadastroRoutes = require("./routes/cadastroRoutes");
+const loginRoutes = require("./routes/loginRoutes");
 
 const PORT = process.env.PORT || 8080;
 
