5becf1062de7e08383c1f93bfdcb446d2ce6ad31
Murilo Dias <diasmurilo02@gmail.com>
2025-02-25 18:24:32 -0300

    Modularização Rotas

--- routes/cadastroRoutes.js
+++ routes/cadastroRoutes.js
@@ -3,20 +3,25 @@ const bcrypt = require("bcrypt");
 const pool = require("../config/db");
 
 const router = express.Router();
+router.use(express.json()); // Adicionando o middleware JSON
 
 // Rota de Cadastro
 router.post("/", async (req, res) => {
     const { nome, email, senha, telefone } = req.body;
     try {
         const result = await pool.query("SELECT * FROM usuarios WHERE nome = $1 OR email = $2", [nome, email]);
+
         if (result.rows.length > 0) {
             return res.status(400).json({ error: "Usuário ou e-mail já existe" });
         }
+
         const senhaCriptografada = await bcrypt.hash(senha, 10);
+
         await pool.query(
             "INSERT INTO usuarios (nome, email, senha, telefone, tipo) VALUES ($1, $2, $3, $4, 9)",
             [nome, email, senhaCriptografada, telefone]
         );
+
         res.status(201).json({ message: "Usuário cadastrado com sucesso" });
     } catch (error) {
         console.error("Erro no cadastro:", error);
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.set("trust proxy", 1);
-
 app.use(express.json());
+
 app.use("/cadastro", cadastroRoutes);
 app.use("/login", loginRoutes);
 
